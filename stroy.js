1da2eb5767c1cea6fa9d4fa9a82c045c989b389a
dharikamal <76258348+dharikamal@users.noreply.github.com>
2021-02-01 19:17:35 +0300

    solved

---
+++ stroy.js
@@ -0,0 +1,6 @@
+let greeting = "Mshary";
+let age = 33;
+let hobby = "Meditation";
+
+console.log("Mr ",greeting, "in",age, "years",+ " you'll be ",+(33 + 25),+
+ "old.", " By then you'll have decades of",hobby, " practice!");
